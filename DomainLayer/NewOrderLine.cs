eaab3282d7d11f2cecfcedbe328842d8b22bfe31
Julian <juli11c0@edu.eal.dk>
2019-02-26 13:31:53 +0100

    Oprettet NewOrderLine Klassen

---
+++ DomainLayer/NewOrderLine.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainLayer
+{
+	public class NewOrderLine
+	{
+		private int quantity;
+		private float price;
+
+		public int Quantity { get { return this.quantity} set { this.quantity = value; } }
+		public float Price { get { return this.price; } set { this.price = value; } }
+
+		public NewOrderLine(int quantity, float price)
+		{
+			Quantity = quantity;
+			Price = price;
+		}
+
+	}
+}

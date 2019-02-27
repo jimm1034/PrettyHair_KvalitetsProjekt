253359f2ae57765934d6009eb96198d170f645a6
Jaroslaw Ireneusz Lorenc <jaro0013@edu.eal.dk>
2019-02-27 12:16:38 +0100

    productrepo tilføjet(??)

---
+++ ApplicationLayer/ProductRepo.cs
@@ -0,0 +1,23 @@
+﻿using DomainLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationLayer
+{
+    public  class ProductRepo
+    {
+        private List<Product> products = new List<Product>();
+
+        public void AddNewProduct(Product product)
+        {
+            products.Add(product);
+        }
+        public void RemoveProduct(Product product)
+        {
+            products.Remove(product);
+        }
+    }
+}

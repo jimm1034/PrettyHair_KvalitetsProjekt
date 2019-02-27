f9bc2ea973076fb9166e43f1292267897ea102b2
Anders Weiskvist <Weiskvist94@gmail.com>
2019-02-27 12:58:28 +0100

    OrderRepo done

---
+++ ApplicationLayer/OrderRepo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DomainLayer;
+namespace ApplicationLayer
+{
+    public class OrderRepo
+    {
+        private List<Order> orders = new List<Order>();
+
+        public void AddNewOrder(Order order)
+        {
+            orders.Add(order);
+        }
+        public void RemoveOrder(Order order)
+        {
+            orders.Remove(order);
+        }
+    }
+}
